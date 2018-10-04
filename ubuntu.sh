26eb3b70cb6db820b6b9f3b73b2758591d1bb759
HenriqueMorato <riquemorato@gmail.com>
2018-10-04 13:54:43 -0300

    Add customizations softwares

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,12 @@
 sudo apt-get update
 sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
 sudo apt-get update
 sudo apt-get install -y silversearcher-ag \
  zsh \
@@ -11,6 +17,16 @@ sudo apt-get install -y silversearcher-ag \
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
 ~/.cc_dotfiles/gnome-terminal-colors-solarized/install.sh
 sudo apt-get purge ruby
+echo 'Installing Google Chrome'
+sudo apt-get install -y sudo apt-get install google-chrome-stable
+echo 'Installing Spotify'
+sudo apt-get install -y spotify-client
+echo 'Installing Rambox'
+wget "https://getrambox.herokuapp.com/download/linux_64?filetype=deb" -O rambox.deb
+sudo dpkg -i rambox.deb
+sudo apt-get install -f
+sudo rm -f rambox.deb
+
 echo 'Change your terminal window to Run command as login shell and restart'
 echo 'You can find more information about this on' \
      'https://github.com/rvm/ubuntu_rvm'
