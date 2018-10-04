e9107560e3ef4aeff61c59733a12701cb00ef092
HenriqueMorato <riquemorato@gmail.com>
2018-10-04 15:37:37 -0300

    Customize ubuntu colors

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository -y ppa:pi-rho/dev
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt-get update
@@ -17,15 +17,19 @@ sudo apt-get install -y silversearcher-ag \
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
 ~/.cc_dotfiles/gnome-terminal-colors-solarized/install.sh
 sudo apt-get purge ruby
-echo 'Installing Google Chrome'
-sudo apt-get install -y sudo apt-get install google-chrome-stable
-echo 'Installing Spotify'
-sudo apt-get install -y spotify-client
+
+echo '===========Installing Google Chrome==========='
+sudo apt install google-chrome-stable
+echo '=========== Installing Spotify ==========='
+sudo apt install spotify-client
 echo 'Installing Rambox'
 wget "https://getrambox.herokuapp.com/download/linux_64?filetype=deb" -O rambox.deb
+wget $(curl -s https://api.github.com/repos/ramboxapp/community-edition/releases | grep browser_download_url | grep '64[.]deb' | head -n 1 | cut -d '"' -f 4) -O rambox.deb
 sudo dpkg -i rambox.deb
 sudo apt-get install -f
 sudo rm -f rambox.deb
+echo 'Changing terminal colors'
+bash ./ubuntu/gnome-terminal-profile import ./ubuntu/profile_colors
 
 echo 'Change your terminal window to Run command as login shell and restart'
 echo 'You can find more information about this on' \
