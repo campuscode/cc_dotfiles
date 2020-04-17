c68bc29a437f6c83e0d199dea8ce6b31568450a0
Alan Batista <alanrrb@gmail.com>
2020-04-17 14:09:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ then
 
   case "$(uname -s)" in
     Linux)
-      sudo apt-get install -y git curl gnupg build-essential
-      gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+      sudo apt-get install -y git curl gnupg2 build-essential
+      gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
       curl -sSL https://get.rvm.io | bash -s stable --ruby
       sudo usermod -a -G rvm `whoami`
       sudo apt-get install ruby
