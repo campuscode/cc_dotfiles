9a8c116cf1d63e45e4c826090f78908be33e4161
Alan Batista <alanrrb@gmail.com>
2020-04-18 11:58:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ then
 
   case "$(uname -s)" in
     Linux)
+      sudo apt-get update
       sudo apt-get install -y git curl gnupg2 build-essential
       gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
       curl -sSL https://get.rvm.io | bash -s stable --ruby
