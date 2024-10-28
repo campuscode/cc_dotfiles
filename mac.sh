68737ac0f8b00f209872375df6035617151b1164
Débora Fernandes <debborafernandess@gmail.com>
2024-10-28 12:22:34 -0400

    Update mac.sh
    Cask isn't a command anymore, was turned into option "--cask"

--- mac.sh
+++ mac.sh
@@ -3,7 +3,7 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh ctags git hub tmux reattach-to-user-namespace the_silver_searcher
 brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit
-brew cask install atom
-brew cask install google-chrome
-brew cask install iterm2
+brew install --cask atom
+brew install --cask google-chrome
+brew install --cask iterm2
 
