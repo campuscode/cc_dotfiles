3d5881e88848d5ff0b720d28e737525800fe0f0b
Alan Rafael R. Batista <alan@campuscode.com.br>
2026-03-21 16:19:56 -0300

    Update mac.sh: fix Homebrew installer, replace deprecated packages
    - Update Homebrew install command to current method
    - Replace hub with gh (GitHub CLI)
    - Remove reattach-to-user-namespace (unnecessary since tmux 2.6+)
    - Remove macvim obsolete flags
    - Replace Atom (discontinued) with VS Code
    - Use brew install --cask (replaces deprecated brew cask install)

--- mac.sh
+++ mac.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh ctags git hub tmux reattach-to-user-namespace the_silver_searcher
-brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit
-brew cask install atom
-brew cask install google-chrome
-brew cask install iterm2
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install zsh ctags git gh tmux the_silver_searcher macvim
+brew install --cask visual-studio-code google-chrome iterm2
