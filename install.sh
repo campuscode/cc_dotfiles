0df1a79b4825515487c6c84414a69f58d95950b4
Alan Rafael R. Batista <alan@campuscode.com.br>
2026-03-21 16:14:44 -0300

    Update install.sh: remove system ruby, use mise for rake install
    Activate mise in the shell before running rake install so Ruby
    comes from mise instead of apt. Also fix return/exit, add set -eu,
    remove duplicated dconf-cli, and simplify unsupported OS handling.

--- install.sh
+++ install.sh
@@ -1,64 +1,59 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
-if [ ! -d "$HOME/.cc_dotfiles" ]
-then
-  echo "Installing Campus Code Dotfiles"
-  echo "We'll install:"
-  echo "  - tmux"
-  echo "  - silver searcher"
-  echo "  - zsh"
-  echo "  - mise (Ruby, Node.js)"
-
-  case "$(uname -s)" in
-    Linux)
-      echo "  - vim (vim-gtk3)"
-      echo "  - Docker"
-      echo "  - docker-compose"
-
-      sudo apt-get update
-      sudo apt-get install -y software-properties-common dconf-cli uuid-runtime
-
-      sudo apt-get install -y silversearcher-ag \
-        git \
-        xclip \
-        build-essential \
-        zsh \
-        dconf-cli \
-        vim-gtk3 \
-        ruby \
-        libevent-dev \
-        ncurses-dev \
-        bison \
-        pkg-config
-
-      curl https://mise.run | sh
-      ~/.local/bin/mise install ruby node
-      ;;
-    Darwin )
-      curl https://mise.run | sh
-      ~/.local/bin/mise install ruby node
-      ;;
-    CYGWIN* | MSYS*)
-      echo 'You are using a Windows machine which is not recommended to use with our' \
-           ' dotfiles.'
-      echo 'You can clone our repository and try install it manually.'
-      return
-      ;;
-    *)
-      echo 'Operational system not recognized, aborting installation'
-      return
-      ;;
-  esac
-  if [ -z "$LOCAL_INSTALL" ]
-  then
-    echo "FROM REMOTE SOURCE"
-    git clone --depth=10 https://github.com/campuscode/cc_dotfiles.git "$HOME/.cc_dotfiles"
-  else
-    echo "LOCAL SOURCE"
-    cp -r . "$HOME/.cc_dotfiles"
-  fi
-  cd "$HOME/.cc_dotfiles"
-  rake install
-else
+if [ -d "$HOME/.cc_dotfiles" ]; then
   echo "You already have Campus Code Dotfiles installed."
+  exit 0
 fi
+
+echo "Installing Campus Code Dotfiles"
+echo "We'll install:"
+echo "  - tmux"
+echo "  - silver searcher"
+echo "  - zsh"
+echo "  - mise (Ruby, Node.js)"
+
+case "$(uname -s)" in
+  Linux)
+    echo "  - vim (vim-gtk3)"
+
+    sudo apt-get update
+    sudo apt-get install -y software-properties-common dconf-cli uuid-runtime
+
+    sudo apt-get install -y silversearcher-ag \
+      git \
+      xclip \
+      build-essential \
+      zsh \
+      vim-gtk3 \
+      libevent-dev \
+      ncurses-dev \
+      bison \
+      pkg-config
+
+    curl https://mise.run | sh
+    eval "$(~/.local/bin/mise activate bash)"
+    mise install ruby node
+    ;;
+  Darwin)
+    curl https://mise.run | sh
+    eval "$(~/.local/bin/mise activate bash)"
+    mise install ruby node
+    ;;
+  *)
+    echo "Operational system not supported, aborting installation"
+    exit 1
+    ;;
+esac
+
+if [ -z "${LOCAL_INSTALL:-}" ]; then
+  echo "Installing from remote source"
+  git clone --depth=10 https://github.com/campuscode/cc_dotfiles.git "$HOME/.cc_dotfiles"
+else
+  echo "Installing from local source"
+  cp -r . "$HOME/.cc_dotfiles"
+fi
+
+cd "$HOME/.cc_dotfiles"
+rake install
+
